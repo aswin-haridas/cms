78ae77c46344f0c0d127069662199a26fde74b2a
aswin-haridas <aswinharidas0@gmail.com>
2025-05-21 23:37:22 +0530

    fix: clean up import statements and formatting in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {  LoginPage, SignupPage,  } from "./pages/auth";
+import { LoginPage, SignupPage } from "./pages/auth";
+import Test from "./pages/test/test";
 
 function App() {
-
-
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage  />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
